ae621176c7b54e1f925c0705e0cc50891babb084
maaz-codes <maazkhan7454@gmail.com>
2024-08-12 00:13:07 +0400

    added the neccessary function prototypes

--- pushswap.h
+++ pushswap.h
@@ -5,6 +5,8 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
+# include <unistd.h>
 
 
 
@@ -15,11 +17,12 @@ typedef struct doubly_list
 	struct doubly_list	*next;
 }						stack;
 
+// stack_utils
 stack					*stack_new(int number);
 stack					*last_block(stack *a);
 void					stack_add(stack **a, stack *new_block);
 int						stack_len(stack *a);
-stack					*stack_init(int *numbers, int len);
+char					*stack_init(stack **a, char **strings, int len);
 
 // instruct utils
 void					swap(stack **a);
@@ -44,6 +47,7 @@ void					rrr(stack **a, stack **b);
 char					**ft_split(const char *s, char c);
 int						ft_word_count(const char *s, char sep);
 int						ft_abs(int num);
+char					*ft_atol_modified(char *str, long *number);
 
 // sorting functions
 stack					*find_max_block(stack *a);
@@ -55,7 +59,22 @@ stack					*target_block(stack *element, stack *s, char stacks);
 stack					*cheapest_block(stack *a, stack *b);
 int						index_to_top(stack *element, stack *s);
 int						push_cost(stack *a, stack *b, stack *element, stack *target);
+void					sorting(stack **a, stack **b);
+int						is_sorted(stack *a);
+
+// pushing cheapest element to top
 void					push_cheapest_element(stack **a, stack **b);
+void					elements_to_top(stack *cheapest, stack *target, stack **a, stack **b);
+void					rotate_ab(stack **a, stack **b, stack *cheapest, stack *target);
+void					reverse_rotate_ab(stack **a, stack **b, stack *cheapest, stack *target);
+void					rotate_a(stack **a, stack *target);
+void					reverse_rotate_a(stack **a, stack *target);
+void					rotate_b(stack **b, stack *cheapest);
+void 					reverse_rotate_b(stack **b, stack *cheapest);
+
+// Errors
+char					*repetition_error(stack **a, long number);
+int						error_msg();
 
 // to remove
 void					print_stack(stack *a);
